c685d7bff8e39d2ce381f634c9f5268a8d385155
junxijunxi <79365235+junxijunxi@users.noreply.github.com>
2023-08-29 16:32:37 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cl74tw7rkmcw.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "heahjunxi-employee"
+customregion = "us-east-1"
+
